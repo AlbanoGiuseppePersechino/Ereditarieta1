3a8b8838123cff7c4d277e125db8930fd6c05d5a
AlbanoGiuseppePersechino <albano.persechino@gmail.com>
2024-04-10 14:17:20 +0200

    Esercizio eredita 1

---
+++ src/Animale.java
@@ -0,0 +1,6 @@
+class Animale {
+    // Metodo per fare il verso dell'animale
+    public void faiIlVerso() {
+        System.out.println("Verso dell'animale");
+    }
+}
---
+++ src/Gatto.java
@@ -0,0 +1,7 @@
+class Gatto extends Animale {
+    // Override del metodo faiIlVerso per far miagolare il gatto
+    @Override
+    public void faiIlVerso() {
+        System.out.println("Il gatto fa: Miaooo");
+    }
+}
